64059bcf7427afbe7e3ca97d58fdcfa421e62ccc
mariapereiro <pereirogonzalezmaria@gmail.com>
2025-03-08 11:56:51 +0100

    Instalacion por ansible y docker

--- ftp.sh
+++ ftp.sh
@@ -98,184 +98,25 @@ menu_logs() {
         echo "Opción no válida. Intentalo de nuevo"
         menu_logs
     fi
-
-    
 }
 
-
-menu_comandos() {
-    echo "Instalación servicio FTP."
-    echo "--menu: Elige la opción"
-    echo "--help: Muestra la ayuda del programa."
-}
-
-menu_docker() {
-    echo "Instalación servicio FTP (Docker)."
-    echo "--instalacion_docker: Instala el servicio."
-    echo "--eliminar_docker: Elimina el servicio."
-    echo "--stop_docker: Parada del servicio."
-    echo "--logs_docker: Muestra los logs."
-}
-
-instalar_servicio_docker() {
-    echo "Creando Dockerfile y construyendo imagen para vsftpd..."
-    cat <<EOF > Dockerfile
-FROM ubuntu:latest
-RUN apt-get update && \
-    apt-get install -y vsftpd && \
-    apt-get clean
-COPY vsftpd.conf /etc/vsftpd.conf
-RUN useradd -m -d /home/usuario_ftp -s /usr/sbin/nologin usuario_ftp && \
-    echo "usuario_ftp:pass_ftp" | chpasswd
-RUN mkdir -p /home/usuario_ftp/ftp_files && \
-    chown usuario_ftp:usuario_ftp /home/usuario_ftp/ftp_files && \
-    chmod 755 /home/usuario_ftp
-EXPOSE 21 
-CMD ["vsftpd", "-o", "listen=NO", "-o", "listen_ipv6=YES"]
-EOF
-    docker build -t vsftpd_server .
-    echo "Imagen Docker creada con éxito. Para ejecutarla usa: docker run -d -p 21:21 vsftpd_server"
-}
-
-eliminar_servicio_docker() {
-  echo "Eliminando contenedor vsftpd..."
-  docker rm -f vsftpd_server || echo "No hay contenedor en ejecución."
-  docker rmi -f vsftpd_server || echo "No hay imagen de Docker creada."
-  echo "Servicio Docker eliminado correctamente."
-}
-
-detener_servicio_docker() {
-    echo "Deteniendo contenedor vsftpd..."
-    docker stop vsftpd_server || echo "El contenedor ya está detenido."
-}
-
-mostrar_logs_docker() {
-    echo "Mostrando logs del contenedor FTP..."
-    docker logs vsftpd_server
-}
-
-menu_ansible() {
-    echo "Instalación servicio FTP (con Ansible)."
-    echo "--menu_ansible: Muestra las opciones del servicio."
-    echo "--help_ansible: Muestra la ayuda del programa."
-}
-
-
-
-instalar_servicio_comandos() {
-    echo "Instalando el servicio FTP..."
+instalar_con_comandos() {
+    echo "Instalando el servicio FTP con COMANDOS..."
     sudo apt update && sudo apt install -y vsftpd
-    echo "Instalación completada."
+    echo "INSTALACIÓN por comandos completada"
+    menu_principal
 }
 
-eliminar_servicio_comandos() {
-    echo "Eliminando el servicio FTP por completo..."
-    sudo apt purge -y vsftpd && sudo apt autoremove -y
-    sudo rm -rf /etc/vsftpd /var/log/vsftpd.log /srv/ftp /home/ftp
-    echo "Servicio eliminado completamente."
+instalar_con_ansible() {
+    echo "Instalando el servicio FTP con ANSIBLE..."
+    # AQUI VA EL ANSIBLE (instalación)
+    echo "INSTALACIÓN por ansible completada"
+    menu_principal
 }
 
-ejecutar_servicio_comandos() {
-    echo "Iniciando servicio FTP..."
-    sudo systemctl start vsftpd
-    echo "El servicio ya está activo."
-} 
-
-parar_servicio_comandos() {
-    echo "Deteniendo servicio FTP..."
-    sudo systemctl stop vsftpd
-    echo "El servicio está detenido."
-} 
-
-crear_usuario() {
-    read -r -p "Introduce el nombre de tu nuevo usuario: " usuario
-    sudo useradd -m -d /home/$usuario -s /usr/sbin/nologin $usuario
-    sudo passwd $usuario
-    echo "Usuario $usuario creado correctamente. Su carpeta estará en /home/$usuario."
+instalar_con_docker() {
+    echo "Instalando el servicio FTP con DOCKER..."
+    # AQUI VA EL CONTENEDOR DE DOCKER"
+    echo "INSTALACIÓN por docker completada"
+    menu_principal
 }
-
-eliminar_usuario() {
-    read -r -p "Introduce el nombre de usuario que quieres eliminar: " usuario
-    sudo userdel -r $usuario
-    echo "Usuario $usuario eliminado junto con su carpeta."
-}
-if [ "$1" == "--menu_principal" ]; then
-  menu_principal
-  exit 0
-fi
-
-if [ "$1" == "--comandos" ]; then
-    menu_comandos
-    exit 0
-fi
-
-if [ "$1" == "--help" ]; then
-    menu_comandos
-    exit 0
-fi
-
-if [ "$1" == "--menu" ]; then
-    comandos_opciones
-    exit 0
-fi
-
-if [ "$1" == "--instalacion" ]; then
-    instalar_servicio_comandos
-    exit 0
-fi
-
-if [ "$1" == "--eliminar" ]; then
-    eliminar_servicio_comandos
-    exit 0
-fi
-
-if [ "$1" == "--ejecutar" ]; then
-    ejecutar_servicio_comandos
-    exit 0
-fi
-
-if [ "$1" == "--stop" ]; then
-    parar_servicio_comandos
-    exit 0
-fi
-
-if [ "$1" == "--crear_usuario" ]; then
-    crear_usuario
-    exit 0
-fi
-
-if [ "$1" == "--eliminar_usuario" ]; then
-    eliminar_usuario
-    exit 0
-fi
-
-
-if [ "$1" == "--Docker" ]; then
-    menu_docker
-    exit 0
-fi
-
-if [ "$1" == "--instalacion_docker" ]; then
-    instalar_servicio_docker
-    exit 0
-fi
-
-if [ "$1" == "--eliminar_docker" ]; then
-    eliminar_servicio_docker
-    exit 0
-fi
-
-if [ "$1" == "--stop_docker" ]; then
-    detener_servicio_docker
-    exit 0
-fi
-
-if [ "$1" == "--logs_docker" ]; then
-    mostrar_logs_docker
-    exit 0
-fi
-
-else
-  echo "Opción no válida. Usa --help para mostrar la ayuda del menú."
-  menu_principal
-
